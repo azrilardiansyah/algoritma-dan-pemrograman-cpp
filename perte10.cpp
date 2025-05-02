fb761539eba27e9c31afda0d6e61dcc76c528316
azrilardiansyah <azrilardiansyah27@gmail.com>
2025-05-02 19:19:30 +0700

    Add files via upload

---
+++ perte10.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c, d;
+	
+	cout<<" Masukan 4 buah bilangan bulat : ";
+	cin>>a>>b>>c>>d;
+	
+	int terbesar = a;
+	
+	if ( b > terbesar) {
+		terbesar = b;
+	}
+	if ( c > terbesar) {
+		terbesar = c;
+	}
+	if ( d > terbesar) {
+		terbesar = d;
+	}
+	
+	cout<<" Bilangan terbesar adalah : "<<terbesar<<endl;
+	
+	return 0;
+}
